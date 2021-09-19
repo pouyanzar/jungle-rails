5f4d65e49713fee65b3a721123e87f6b707b1909
pouyanzar <pouyan.zarbafian@hotmail.com>
2021-09-19 16:47:45 +0000

    add user authentication

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+  before_filter :authorize
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
+  end
+
+  def new
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+end
